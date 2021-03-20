1171e8a13414ee558e76cad080564cc4898ec7a9
ButterPy <mydearseliisa@gmail.com>
2021-03-20 14:06:46 +0600

    .

---
+++ Algorithms/1773.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countMatches(self, items, ruleKey, ruleValue):
+        """
+        :type items: List[List[str]]
+        :type ruleKey: str
+        :type ruleValue: str
+        :rtype: int
+        """
+        key = {
+            'type':  0,
+            'color': 1,
+            'name':  2
+        }
+
+        count = 0
+
+        for i in items:
+            if ruleValue in i:
+                if i[key[ruleKey]] == ruleValue:
+                    count += 1
+        return count
